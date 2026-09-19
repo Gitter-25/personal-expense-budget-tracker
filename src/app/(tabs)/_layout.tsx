cd6a39bab9a6175bf3d11b3d80c54163b666192b
John Gitter Perito <johngitterperito@gmail.com>
2026-09-19 17:40:03 +0800

    Move home tab to center

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -3,9 +3,9 @@ import { Tabs } from "expo-router";
 export default function TabsLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
       <Tabs.Screen name="add-expense" options={{ title: "Add Expense" }} />
       <Tabs.Screen name="statistics" options={{ title: "Statistics" }} />
+      <Tabs.Screen name="index" options={{ title: "Home" }} />
       <Tabs.Screen name="budget" options={{ title: "Budget" }} />
       <Tabs.Screen name="settings" options={{ title: "Settings" }} />
     </Tabs>
